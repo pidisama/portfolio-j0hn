7027fb8a8bd24941a2bee739ca75237112929b37
pidisama <53374640+pidisama@users.noreply.github.com>
2019-07-28 03:14:11 +0800

    Update delay.js

--- js/delay.js
+++ js/delay.js
@@ -1,16 +1,3 @@
-
-
-// $(document).ready(function() {
-//   $('#freeze').click(function(e) {
-//     e.preventDefault();
-//     var $a = $(this).addClass('clicked');
-//     setTimeout(function() {
-//       window.location.assign($a.attr('href'));
-//        // window.location.href = self.href;
-//     }, 300);
-//   });
-// });
-
 var delay;
 
 $('a.foo').click(function(e) {
@@ -23,6 +10,6 @@ $('a.foo').click(function(e) {
   
   setTimeout(function() {
   	alert('Still good.');
-  	window.location.href = 'Index.html#freeze';
+  	window.location.href = '#freeze';
   },delay);
 });
